a2ed64e63a14ee29879bbda3aef86c2de5e0d8db
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-05-24 21:11:39 +0300

    Added realization with all possible boards

---
+++ FullGame/game.py
@@ -0,0 +1,69 @@
+from board import Board
+from copy import deepcopy
+
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+
+    def check_input(self, ui):
+        try:
+            ui = eval(ui)
+            if not (isinstance(ui, tuple) or isinstance(ui, list)):
+                raise TypeError
+            elif not (isinstance(ui[0], int) and isinstance(ui[1], int)):
+                raise TypeError
+            elif len(ui) != 2:
+                raise Exception
+            elif self.board.cells[ui[0]][ui[1]] != Board.EMPTY:
+                raise Exception
+            else:
+                assert 0 <= ui[0] <= 2
+                assert 0 <= ui[1] <= 2
+        except (AssertionError, TypeError, NameError, SyntaxError, Exception):
+            return 0
+        else:
+            return 1
+
+    def player_make_move(self, position):
+        if self.board.has_winner():
+            return
+        position = eval(position)
+        self.board.make_move(position)
+
+    def computer_make_move(self):
+        if self.board.has_winner():
+            return
+        possible_moves = list()
+        boards = list()
+        for i in range(3):
+            for j in range(3):
+                if self.board.cells[i][j] == Board.EMPTY:
+                    possible_moves.append((i, j))
+        for _ in range(len(possible_moves)):
+            boards.append(deepcopy(self.board))
+        for b in range(len(boards)):
+            boards[b].make_move(possible_moves[b])
+        max_score_board = max(boards, key=lambda x: x.compute_score())
+        self.board = max_score_board
+
+    @staticmethod
+    def get_winner(code):
+        decoder = {1: 'AI won!', -1: 'User won!', 0: 'Draw.'}
+        return decoder[code]
+
+    def launch(self):
+        while not self.board.has_winner():
+            pl = input('Enter move: ')
+            if self.check_input(pl):
+                self.player_make_move(pl)
+                self.computer_make_move()
+                print(self.board, end='\n\n')
+            else:
+                print('Reenter correctly, please!')
+        print(self.get_winner(self.board.has_winner()))
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.launch()
